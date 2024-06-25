f35ae5359a3904416d31e01184af660effc6865a
James O'Rourke <james@cloudadvantage.com.au>
2024-06-25 15:08:59 +1000

    Save this

--- app/a/app.py
+++ app/a/app.py
@@ -3,20 +3,37 @@ import requests
 from flask import Flask
 
 app = Flask(__name__)
-consul_client = consul.Consul()
+consul_client = consul.Consul(
+    host="dcx-consul-cluster-dev.consul.79ccb6be-44be-4241-a037-e96e565a87ac.aws.hashicorp.cloud",
+    port=443,
+    scheme="https"
+)
 
 
 @app.route('/')
 def hello():
     # Get Service B address from Consul
-    _, service_b_address = consul_client.catalog.service('service-b')[1][0]
+    index, services = consul_client.catalog.service('service-b')
+    if services:
+        service_b = services[0]
+        service_b_address = service_b['ServiceAddress']
+        service_b_port = service_b['ServicePort']
 
-    # Make request to Service B
-    response = requests.get(f'https://{service_b_address}:8081/name')
-    name = response.text
+        # Make request to Service B
+        try:
+            response = requests.get(f'http://service-b-service:8081/name')
+            name = response.text
+        except requests.RequestException as e:
+            print(f"Error connecting to Service B: {e}", flush=True)
+            raise e
+    else:
+        name = "Error: Service B not found in Consul"
 
     return f"Hello, {name}!"
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    print(f"Service A is running on port 8080")
+    details = consul_client.catalog.service('service-b')
+    print(f"details: {details}")
+    app.run(debug=True, port=8080, host='0.0.0.0')
--- app/b/app.py
+++ app/b/app.py
@@ -5,17 +5,24 @@ import consul
 from flask import Flask
 
 app = Flask(__name__)
-consul_client = consul.Consul()
+consul_client = consul.Consul(
+    host="dcx-consul-cluster-dev.consul.79ccb6be-44be-4241-a037-e96e565a87ac.aws.hashicorp.cloud",
+    port=443,
+    scheme="https"
+)
 
 
 @app.route('/name')
 def get_name():
     # Get names from Consul KV store
-    _, names_data = consul_client.kv.get('names-config/names')
-    names = json.loads(names_data['Value'])
+    # _, names_data = consul_client.kv.get('names-config/names')
+
+    with open('/app/config/names', 'r') as f:
+        names = json.load(f)
 
     return random.choice(names)
 
 
 if __name__ == '__main__':
-    app.run(port=8081)
+    print(f"Service B is running on port 8081")
+    app.run(port=8081, host='0.0.0.0')
